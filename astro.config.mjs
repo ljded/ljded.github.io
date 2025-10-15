0da09b5200eaf9b33bb8c07a06acbe368dc95a82
龙剑de刀 <ljded@qq.com>
2025-10-15 18:37:38 +0800

    GitHub pages部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ljded.cn/",
+	site: "https://longjiandedao.cn/",
 
-	base: "/",
+	// base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
