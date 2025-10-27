85aa4f74896d926be28612464922bf4a69a72daf
龙剑de刀 <ljded@qq.com>
2025-10-27 12:47:48 +0800

    修改网站域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://longjiandedao.cn/",
+	site: "https://ljded.cn/",
 
 	// base: "/",
 	trailingSlash: "always",
