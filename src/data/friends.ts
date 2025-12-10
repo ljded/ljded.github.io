12e591f00640ed86743c2b4d07c3c07579c9d637
城倉奏 <admin@thdog.moe>
2025-12-10 22:45:55 +0800

    pnpm format
    (cherry picked from commit b7ca94938c3faeb2124bbfcb504b825d3159140e)

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -2,12 +2,12 @@
 // 用于管理友情链接页面的数据
 
 export interface FriendItem {
-    id: number;
-    title: string;
-    imgurl: string;
-    desc: string;
-    siteurl: string;
-    tags: string[];
+	id: number;
+	title: string;
+	imgurl: string;
+	desc: string;
+	siteurl: string;
+	tags: string[];
 }
 
 // 友情链接数据
@@ -56,15 +56,15 @@ export const friendsData: FriendItem[] = [
 
 // 获取所有友情链接数据
 export function getFriendsList(): FriendItem[] {
-    return friendsData;
+	return friendsData;
 }
 
 // 获取随机排序的友情链接数据
 export function getShuffledFriendsList(): FriendItem[] {
-    const shuffled = [...friendsData];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
+	const shuffled = [...friendsData];
+	for (let i = shuffled.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+	}
+	return shuffled;
 }
